4e58aa63a00defe66c196d72f516e09e8ce3b5b4
Eltonleao <3lton.leao@gmail.com>
2021-01-04 23:13:10 -0300

    ajax connections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
     console.log('is running');
+
+    $.ajax({
+        method: "GET",
+        url: "http://200.141.127.130:210/projetos/lorem-api/FRAMEWORK/test",
+    })
+        .done(function (data) {
+            console.log(data);
+        });
 })
