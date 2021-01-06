5d369434068fd1533fb6f3c89b7f40805872486d
Eltonleao <3lton.leao@gmail.com>
2021-01-06 08:34:19 -0300

    E: ajax comentado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,16 @@
-$(document).ready(function () {
-    console.log('is running');
-    $("#content").html('Carregando...')
+// $(document).ready(function () {
+//     console.log('is running');
+//     $("#content").html('Carregando...')
 
-    $.ajax({
-        method: "GET",
-        url: "https://200.141.127.130:443/projetos/lorem-api/test",
-    })
-    .done(function (data) {
-        data = JSON.parse(data);
-        data = data.result;
-        $("#content").html(data.msg)
+//     $.ajax({
+//         method: "GET",
+//         url: "https://200.141.127.130:443/projetos/lorem-api/test",
+//     })
+//     .done(function (data) {
+//         data = JSON.parse(data);
+//         data = data.result;
+//         $("#content").html(data.msg)
         
 
-    });
-})
+//     });
+// })
